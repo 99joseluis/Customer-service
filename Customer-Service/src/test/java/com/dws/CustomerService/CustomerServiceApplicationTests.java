bc25aae0a04bfaa40b6a66daa0b714af8bc3dff3
99joseluis <jose_luis@ciencias.unam.mx>
2021-05-25 12:37:12 -0500

    first commit

---
+++ Customer-Service/src/test/java/com/dws/CustomerService/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dws.CustomerService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
